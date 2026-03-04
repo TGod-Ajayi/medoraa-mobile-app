482562a76de4105eec790aa4c328175467f8b0b3
Richard Udeh <richardudeh03@gmail.com>
2026-03-04 15:44:39 +0100

    first commit

---
+++ apps/users/app/(tabs)/_layout.tsx
@@ -0,0 +1,38 @@
+import { HapticTab, IconSymbol } from '@repo/ui/components';
+import { Colors } from '@repo/ui/constants';
+import { Tabs } from 'expo-router';
+import { useColorScheme } from 'react-native';
+
+export default function TabLayout() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: (Colors as Record<string, typeof Colors.light>)[
+          colorScheme ?? 'light'
+        ].tint,
+        headerShown: false,
+        tabBarButton: HapticTab,
+      }}>
+      <Tabs.Screen
+        name='index'
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name='house.fill' color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name='explore'
+        options={{
+          title: 'Explore',
+          tabBarIcon: ({ color }) => (
+            <IconSymbol size={28} name='paperplane.fill' color={color} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
